b603c87e80cd79e28de7a635a3e9e634e3aa9e4d
Nazmul Hasan <najmul.hasan10@gmail.com>
2016-03-23 10:32:59 +0600

    initial commit

--- app/src/main/java/com/nerdcastle/nazmulhasan/samplewebservice/MainActivity.java
+++ app/src/main/java/com/nerdcastle/nazmulhasan/samplewebservice/MainActivity.java
@@ -1,96 +1,77 @@
 package com.nerdcastle.nazmulhasan.samplewebservice;
 
-import java.util.ArrayList;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
+import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.Request.Method;
 import com.android.volley.toolbox.StringRequest;
 
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
-import android.widget.AdapterView.OnItemClickListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 
 
-public class MainActivity extends ActionBarActivity {
-	JSONArray addonArray;
-	ArrayList<String> addonsList;
-	ListView listView;
+public class MainActivity extends Activity {
+    String url="http://nerdcastlebd.com/web_service/banglapoems/index.php/poets/all/format/json";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        listView=(ListView) findViewById(R.id.listView);
-        addonsList=new ArrayList<String>();
-        showService();
+        setContentView(R.layout.main);
+        getWebData();
+        mReadJsonData("test");
     }
-    
-    
-    private void showService() {
-    	StringRequest stringrequest = new StringRequest(Method.GET,
-				"http://nerdcastlebd.com/web_service/banglapoems/index.php/poets/all/format/json",
-				new Response.Listener<String>() {
 
-					@Override
-					public void onResponse(String response) {
-						try {
-							JSONObject jsonObject = new JSONObject(response);
-							addonArray =jsonObject.getJSONArray("poets");
-							for (int i = 0; i < addonArray.length(); i++) {
+    private void getWebData() {
+        StringRequest stringrequest = new StringRequest(Request.Method.GET,
+                "http://nerdcastlebd.com/_old-site/web_service/banglapoems/index.php/poets/all/format/json",
+                new Response.Listener<String>() {
 
-							 String addonsName = addonArray.getJSONObject(i)
-										.getString("name");
-							 final String biography = addonArray.getJSONObject(i)
-										.getString("biography");
-								addonsList.add(addonsName);
-								ArrayAdapter<String> adapter=new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1,addonsList);
-								listView.setAdapter(adapter);
-								listView.setOnItemClickListener(new OnItemClickListener() {
+                    @Override
+                    public void onResponse(String response) {
 
-									@Override
-									public void onItemClick(
-											AdapterView<?> parent, View view,
-											int position, long id) {
-									String bio=addonsList.get(position);
-									
-										Toast.makeText(getApplicationContext(), addonsList.get(position),Toast.LENGTH_LONG).show();
-									}
-								});
+                        mCreateAndSaveFile("test",response);
 
-							
+                    }
 
-							}
+                }, new Response.ErrorListener() {
 
-						} catch (JSONException e) {
+            @Override
+            public void onErrorResponse(VolleyError arg0) {
 
-							e.printStackTrace();
-						}
+            }
+        });
 
-					}
+        AppController.getInstance().addToRequestQueue(stringrequest);
 
-				}, new Response.ErrorListener() {
+    }
 
-					@Override
-					public void onErrorResponse(VolleyError arg0) {
-
-					}
-				});
-
-		AppController.getInstance().addToRequestQueue(stringrequest);
-
-		
-	}
-
-
+    public void mCreateAndSaveFile(String params, String mJsonResponse) {
+        try {
+            FileWriter file = new FileWriter("/data/data/" + getApplicationContext().getPackageName() + "/" + params);
+            file.write(mJsonResponse);
+            file.flush();
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void mReadJsonData(String params) {
+        try {
+            File f = new File("/data/data/" + getPackageName() + "/" + params);
+            FileInputStream is = new FileInputStream(f);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            String mResponse = new String(buffer);
+            Toast.makeText(MainActivity.this, mResponse, Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 }
